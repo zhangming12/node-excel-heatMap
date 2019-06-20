a0bde16dea883985d28b2aea1d902f9ab21bf25d
zhangming12 <zhangmingcoder@163.com>
2019-06-20 15:32:34 +0800

    解耦代码，完善错误处理机制

---
+++ tools/isFileExisted.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+/**
+ * 判断文件是否存在
+ * @param {String} fileName //文件路径
+ * @returns {Promise}
+ */
+exports.isFileExisted = function isFileExisted(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.access(fileName, err => {
+      if (err)
+        reject(err.message);
+      resolve();
+
+    })
+  })
+}
